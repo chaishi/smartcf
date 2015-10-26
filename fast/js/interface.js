e9c539683ce58e1671450e1a4400cbaee2938901
luoxue <974383157@qq.com>
2015-10-26 12:02:48 +0800

    解决ie8背景半透明色兼容问题

--- fast/js/interface.js
+++ fast/js/interface.js
@@ -1,16 +1,16 @@
 var interfaces = {};
 
 /*我的桌面html*/
-interfaces.myDeskHtml = '/fast/myDesk.html';
+interfaces.myDeskHtml = 'myDesk.html';
 
 /*日常事务html*/
-interfaces.dailyHtml = '/fast/daily.html';
+interfaces.dailyHtml = 'daily.html';
 
 /*投票html*/
-interfaces.ticketsHtml = '/fast/tickets.html';
+interfaces.ticketsHtml = 'tickets.html';
 
 /*未开发*/
-interfaces.nothingHtml = '/fast/nothing.html';
+interfaces.nothingHtml = 'nothing.html';
 
 /*根据屏幕或id获取app列表*/
 interfaces.getApps = 'json/app.json';
